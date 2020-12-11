6563658f925da92a6a631ca7347b10569bb5220a
Andrey Nasonov <nasonov256@gmail.com>
2020-12-11 13:41:46 +0300

    Add using BenchmarkDotNet

--- src/PerformanceCSharp/Program.cs
+++ src/PerformanceCSharp/Program.cs
@@ -1,22 +1,24 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 [assembly: InternalsVisibleTo("PerformanceCSharp.Test")]
 
 namespace PerformanceTests
 {
-    static unsafe class Program
+    public class Benchmarks
     {
-        const int size = 256;
+        readonly NativeImage<float> img1, img2, kernel, res;
         
-        static void Main(string[] args)
+        const int size = 256;
+        public Benchmarks()
         {
-            PerformanceMeasurement.Run(() => { }, "Empty action");
-
-            var img1 = new NativeImage<float>(size, size);
-            var img2 = new NativeImage<float>(size, size);
-            var kernel = new NativeImage<float>(7, 7);
-            var res = new NativeImage<float>(size, size);
-
+            img1 = new NativeImage<float>(size, size);
+            img2 = new NativeImage<float>(size, size);
+            kernel = new NativeImage<float>(7, 7);
+            res = new NativeImage<float>(size, size);
+            
             for (var j = 0; j < res.Height; j++)
             for (var i = 0; i < res.Width; i++)
             {
@@ -29,22 +31,54 @@ namespace PerformanceTests
             {
                 kernel[i, j] = 1.0f;
             }
+        }
 
-            PerformanceMeasurement.Run(() => ImageOperations.Sum_GetSetMethods(img1, img2, res), "Sum: naive, get/set");
-            PerformanceMeasurement.Run(() => ImageOperations.Sum_RefMethod(img1, img2, res), "Sum: naive, ref pixel");
-            PerformanceMeasurement.Run(() => ImageOperations.Sum_ThisProperty(img1, img2, res), "Sum: naive, this[,]");
-            PerformanceMeasurement.Run(() => ImageOperations.Sum_Optimized(img1, img2, res), "Sum: optimized");
-            PerformanceMeasurement.Run(() => ImageOperations.Sum_Avx(img1, img2, res), "Sum: avx");
-            PerformanceMeasurement.Run(() => ImageOperations.Rotate180(img1, res), "Rotate: naive");
-            PerformanceMeasurement.Run(() => ImageOperations.Rotate180_Optimized(img1, res), "Rotate: optimized");
-            PerformanceMeasurement.Run(() => ImageOperations.Rotate180_Avx(img1, res), "Rotate: avx");
-            PerformanceMeasurement.Run(() => ImageOperations.MedianFilter(img1, 1, res), "Median 3x3");
-            PerformanceMeasurement.Run(() => ImageOperations.MedianFilter(img1, 2, res), "Median 5x5");
-            PerformanceMeasurement.Run(() => ImageOperations.MedianFilter(img1, 3, res), "Median 7x7");
-            PerformanceMeasurement.Run(() => ImageOperations.Convolve(img1, kernel, res), "Convolution: naive");
-            PerformanceMeasurement.Run(() => ImageOperations.Convolve_Optimized(img1, kernel, res), "Convolution: optimized");
-            PerformanceMeasurement.Run(() => ImageOperations.Convolve_Avx(img1, kernel, res), "Convolution: vector, avx");
-            PerformanceMeasurement.Run(() => ImageOperations.Convolve_AvxIsolated(img1, kernel, res), "Convolution [*]: vector, avx");
+        [Benchmark] public void Sum_GetSetMethods() => ImageOperations.Sum_GetSetMethods(img1, img2, res);
+        [Benchmark] public void Sum_RefMethod() => ImageOperations.Sum_RefMethod(img1, img2, res);
+        [Benchmark] public void Sum_ThisProperty() => ImageOperations.Sum_ThisProperty(img1, img2, res);
+        [Benchmark] public void Sum_Optimized() => ImageOperations.Sum_Optimized(img1, img2, res);
+        [Benchmark] public void Sum_Avx() => ImageOperations.Sum_Avx(img1, img2, res);
+        [Benchmark] public void Rotate180() => ImageOperations.Rotate180(img1, res);
+        [Benchmark] public void Rotate180_Optimized() => ImageOperations.Rotate180_Optimized(img1, res);
+        [Benchmark] public void Rotate180_Avx() => ImageOperations.Rotate180_Avx(img1, res);
+        [Benchmark] public void MedianFilter3x3() => ImageOperations.MedianFilter(img1, 1, res);
+        [Benchmark] public void MedianFilter5x5() => ImageOperations.MedianFilter(img1, 2, res);
+        [Benchmark] public void MedianFilter7x7() => ImageOperations.MedianFilter(img1, 3, res);
+        [Benchmark] public void Convolve() => ImageOperations.Convolve(img1, kernel, res);
+        [Benchmark] public void Convolve_Optimized() => ImageOperations.Convolve_Optimized(img1, kernel, res);
+        [Benchmark] public void Convolve_Avx() => ImageOperations.Convolve_Avx(img1, kernel, res);
+        [Benchmark] public void Convolve_AvxIsolated() => ImageOperations.Convolve_AvxIsolated(img1, kernel, res);
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Use BenchmarkDotNet? [y/n]: ");
+            if (Console.ReadLine() is "y" or "Y" or "yes")
+            {
+                BenchmarkRunner.Run<Benchmarks>();
+            }
+            else
+            {
+                var benchmarks = new Benchmarks();
+
+                PerformanceMeasurement.Run(benchmarks.Sum_GetSetMethods, "Sum: naive, get/set");
+                PerformanceMeasurement.Run(benchmarks.Sum_RefMethod, "Sum: naive, ref pixel");
+                PerformanceMeasurement.Run(benchmarks.Sum_ThisProperty, "Sum: naive, this[,]");
+                PerformanceMeasurement.Run(benchmarks.Sum_Optimized, "Sum: optimized");
+                PerformanceMeasurement.Run(benchmarks.Sum_Avx, "Sum: avx");
+                PerformanceMeasurement.Run(benchmarks.Rotate180, "Rotate: naive");
+                PerformanceMeasurement.Run(benchmarks.Rotate180_Optimized, "Rotate: optimized");
+                PerformanceMeasurement.Run(benchmarks.Rotate180_Avx, "Rotate: avx");
+                PerformanceMeasurement.Run(benchmarks.MedianFilter3x3, "Median 3x3");
+                PerformanceMeasurement.Run(benchmarks.MedianFilter5x5, "Median 5x5");
+                PerformanceMeasurement.Run(benchmarks.MedianFilter7x7, "Median 7x7");
+                PerformanceMeasurement.Run(benchmarks.Convolve, "Convolution: naive");
+                PerformanceMeasurement.Run(benchmarks.Convolve_Optimized, "Convolution: optimized");
+                PerformanceMeasurement.Run(benchmarks.Convolve_Avx, "Convolution: vector, avx");
+                PerformanceMeasurement.Run(benchmarks.Convolve_AvxIsolated, "Convolution [*]: vector, avx");
+            }
         }
     }
 }
